e5a3de530987ff3bb45c46d5411f13fb3747f979
miftahuljannahhh <mj5416386@gmail.com>
2025-05-22 09:56:34 +0700

    class stack

---
+++ Stack.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+// Node class representing a single node in the linked list
+class Node{
+public:
+ int data;
+ Node* next;
+
+ Node() {
+    next = NULL;
+ }
+};
+
+// Stack class
+class Stack{
+private:
+ Node* top; // Pointer to the top node of the stack
+
+ public:
+  Stack(){
+   top = NULL; //Initialize the stack with a null top pointer
+ }
+
+ 
+
+};
