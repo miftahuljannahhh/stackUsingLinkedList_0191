c72b163566fc9a9c8f9cd643188886229c742804
miftahuljannahhh <mj5416386@gmail.com>
2025-05-22 11:05:04 +0700

    menambahkan delete temp

--- Stack.cpp
+++ Stack.cpp
@@ -42,6 +42,7 @@ private:
    Node* temp = top; // Create a temporary pointer to the top node
    top = top->next; // Update the top pointer to the next node
    cout << "Popped value:" << top->data << endl;
+   delete temp;
  }
 
  // Peek/Top operation: Retrieve the value of the tompost element without removing
