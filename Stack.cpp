04b7fa99fd01950e9da2aca454dbaae988dd212e
miftahuljannahhh <mj5416386@gmail.com>
2025-05-22 10:57:45 +0700

    No top value

--- Stack.cpp
+++ Stack.cpp
@@ -108,6 +108,12 @@ int main()
          {
             stack.peek(); // Get the value of the top element 
          }
+         else
+         {
+            cout << "Stack is empty. No top value." << endl;
+         }
+         break;
+
       }
 }
 
