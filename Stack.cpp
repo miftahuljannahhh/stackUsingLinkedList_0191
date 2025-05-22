ff4f82a5268bac0e5ed606885d99fade61c1a1ff
miftahuljannahhh <mj5416386@gmail.com>
2025-05-22 10:56:34 +0700

    case 3

--- Stack.cpp
+++ Stack.cpp
@@ -103,6 +103,11 @@ int main()
             cout << "Stack is empty. Cannot pop." << endl;
          }
          break;
+         case 3:
+         if (!stack.isEmpty())
+         {
+            stack.peek(); // Get the value of the top element 
+         }
       }
 }
 
